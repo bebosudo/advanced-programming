57261268850972f391a5bfcf50fd4fb08e90e364
bebo_sudo <bebo.sudo@gmail.com>
2017-10-21 19:18:51 +0200

    Done week 2 homeworks.

--- exercises/02_arrays/primes/primes.cc
+++ exercises/02_arrays/primes/primes.cc
@@ -4,39 +4,42 @@
 */
 
 #include <iostream>
-#include <math.h>
-#include <string>
 
-const short int MAX_PRIME = 100;
+const int NUM_PRIMES_TO_SEARCH = 1000000;
+
+const int MAX_PRIMES = NUM_PRIMES_TO_SEARCH / 2;
 
 int main(int argc, char const *argv[]) {
-  int primes_seen[MAX_PRIME / 2];
+  int primes_seen[MAX_PRIMES] = {0};
+
+  primes_seen[0] = 2;
   int last_prime_seen_idx = 0;
 
-  int prime_already_seen_idx = 0;
+  int s = 0;
 
-  for (int number_test_prime = 2; number_test_prime < MAX_PRIME;
-       number_test_prime += 2) {
+  for (int num_to_test = 3; num_to_test < NUM_PRIMES_TO_SEARCH;
+       num_to_test += 2) {
+    bool is_n_prime = true;
+    // std::cout << "number to test prime = " << num_to_test << std::endl;
 
-    std::cout << "number_test_prime = " << number_test_prime << std::endl;
+    for (s = 0; s < last_prime_seen_idx; ++s) {
 
-    for (prime_already_seen_idx = 0;
-         prime_already_seen_idx < last_prime_seen_idx;
-         prime_already_seen_idx++) {
-
-      std::cout << "prime_already_seen_idx = " << prime_already_seen_idx
-                << std::endl;
-
-      if (number_test_prime % primes_seen[prime_already_seen_idx] == 0) {
-        primes_seen[last_prime_seen_idx] = number_test_prime;
+      if (num_to_test % primes_seen[s] == 0) {
+        is_n_prime = false;
+        break;
       }
+    }
+
+    if (is_n_prime) {
+      primes_seen[last_prime_seen_idx] = num_to_test;
+      ++last_prime_seen_idx;
     }
   }
 
-  std::cout << "ciaone" << std::endl;
-  for (int i = 0; i < last_prime_seen_idx; i++) {
-    std::cout << "-" << primes_seen[i] << std::endl;
-  }
+  // Print out the prime numbers up to NUM_PRIMES_TO_SEARCH.
+  for (s = 0; s < last_prime_seen_idx; ++s)
+    std::cout << primes_seen[s] << " ";
 
+  std::cout << std::endl;
   return 0;
 }
