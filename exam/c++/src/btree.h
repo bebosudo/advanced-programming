e3b9582fa6a1d988b2a3149779c3992a60126baf
Francesco Franchina <cescus92@gmail.com>
2018-02-22 00:52:57 +0100

    BTree skeleton

---
+++ exam/c++/src/btree.h
@@ -0,0 +1,25 @@
+//#include <stdlib>
+
+template <typename K, typename V, typename cmp>
+class BTree {
+    private:
+        std::unique_ptr<int> root;
+
+        class Node;
+
+    public:
+        BTree() {};
+
+        //insert(std::pair<K, V>);
+
+};
+
+template <typename K, typename V, typename cmp>
+class BTree<K, V, cmp>::Node {
+    public:
+        K key;
+        V value;
+        std::unique_ptr<BTree::Node> left, right;
+
+        Node(std::pair<K, V> pair): key{pair.first}, value{pair.second} {};
+};
