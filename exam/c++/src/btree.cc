67ca4ecaf023a2649679d20f52ddfbdb7fb9666e
Francesco Franchina <cescus92@gmail.com>
2018-03-02 07:37:12 +0100

    Benchmark script and things

--- exam/c++/src/btree.cc
+++ exam/c++/src/btree.cc
@@ -8,6 +8,14 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(bestbst, m) {
 
+    py::class_<BTree<int, int>::iterator>(m, "iterator")
+
+        .def(py::init<BTree<int, int> *>())
+        
+        .def("__eq__", &BTree<int, int>::iterator::operator==)
+        ;
+
+
     py::class_<BTree<int, int>>(m, "BTree")
 
         .def(py::init<>())
@@ -17,10 +25,21 @@ PYBIND11_MODULE(bestbst, m) {
         .def("size", &BTree<int, int>::size)
         .def("clear", &BTree<int, int>::clear)
         .def("erase", &BTree<int, int>::erase)
+        .def("find", &BTree<int, int>::find)
         .def("is_balanced", &BTree<int, int>::is_balanced)
         .def("balance", &BTree<int, int>::balance)
         .def("height", py::overload_cast<>(&BTree<int, int>::height))
 
+        .def("begin", &BTree<int, int>::begin)
+        .def("end", &BTree<int, int>::end)
+
+        .def("__len__", &BTree<int, int>::size)
+
+/*
+        .def("__iter__", [](BTree<int, int> &t) {
+            return py::make_key_iterator( t.begin(), t.end());
+        }, py::keep_alive<0, 1>())         
+*/
         ;
 
 }
--- exam/mix/benchmarks.py
+++ exam/mix/benchmarks.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-import timeit
+#import timeit
+import time
 import random
 
 import bestbst
@@ -11,26 +12,25 @@ random.seed(42)
 def insert_once(tree):
     tree.insert(random.randint(-10000, 10000), 1234)
 
+def execute_benchmark(tree, rep):
+    start = time.process_time()
+    for _ in range(rep):
+        tree.find(random.randint(-10000, 10000))
+    end = time.process_time()
 
-def test_iterate_linearly(self):
-    last_seen = None
-
-    for pair in self.tree:
-        if not last_seen:
-            last_seen = pair.key()
-
-        else:
-            self.assertGreater(pair.key(), last_seen)
-
+    print("Size: ", len(tree))
+    print("Height: ", tree.height())
+    print("Find benchmark: ", end - start)
+    
 
 if __name__ == "__main__":
     tree = bestbst.BTree()
-    for _ in range(1000):
+    for _ in range(100000000):
         insert_once(tree)
 
-    time_before = timeit.timeit('tree.find(random.randint(-10000, 10000))', number=10000)
+    rep = 1000000
+    
+    execute_benchmark(tree, rep)
     tree.balance()
-    time_after = timeit.timeit('tree.find(random.randint(-10000, 10000))', number=10000)
+    execute_benchmark(tree, rep)
 
-    print("Before balancing it took: {} seconds to find each number.".format(time_before))
-    print("After balancing it took: {} seconds to find each number.".format(time_after))
