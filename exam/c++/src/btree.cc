347369163e880b5e018be592618432c421ef1b4a
Francesco Franchina <cescus92@gmail.com>
2018-03-02 05:56:53 +0100

    Set up the source to use pybind11

--- exam/c++/src/btree.cc
+++ exam/c++/src/btree.cc
@@ -1,1 +1,28 @@
 #include "btree.h"
+
+#ifdef PYTHON_BUILD
+
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(bestbst, m) {
+
+    py::class_<BTree<int, int>>(m, "BTree")
+
+        .def(py::init<>())
+
+        .def("insert", py::overload_cast<const int&, const int&>(&BTree<int, int>::insert))
+        .def("print", &BTree<int, int>::print)
+        .def("size", &BTree<int, int>::size)
+        .def("clear", &BTree<int, int>::clear)
+        .def("erase", &BTree<int, int>::erase)
+        .def("is_balanced", &BTree<int, int>::is_balanced)
+        .def("balance", &BTree<int, int>::balance)
+        .def("height", py::overload_cast<>(&BTree<int, int>::height))
+
+        ;
+
+}
+
+#endif
