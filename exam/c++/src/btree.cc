c19d31102f4d296ef3661e9a2d31dbabbbf42af3
Alberto Chiusole <bebo.sudo@gmail.com>
2018-03-04 12:40:53 +0100

    Fix indentation in pybind source.

--- exam/c++/src/btree.cc
+++ exam/c++/src/btree.cc
@@ -7,20 +7,17 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(bestbst, m) {
-
     py::class_<BTree<int, int>::iterator>(m, "iterator")
 
         .def(py::init<BTree<int, int> *>())
-        
-        .def("__eq__", &BTree<int, int>::iterator::operator==)
-        ;
 
+        .def("__eq__", &BTree<int, int>::iterator::operator==);
 
     py::class_<BTree<int, int>>(m, "BTree")
 
         .def(py::init<>())
 
-        .def("insert", py::overload_cast<const int&, const int&>(&BTree<int, int>::insert))
+        .def("insert", py::overload_cast<const int &, const int &>(&BTree<int, int>::insert))
         .def("print", &BTree<int, int>::print)
         .def("size", &BTree<int, int>::size)
         .def("clear", &BTree<int, int>::clear)
@@ -35,13 +32,12 @@ PYBIND11_MODULE(bestbst, m) {
 
         .def("__len__", &BTree<int, int>::size)
 
-/*
+        /*
         .def("__iter__", [](BTree<int, int> &t) {
             return py::make_key_iterator( t.begin(), t.end());
-        }, py::keep_alive<0, 1>())         
-*/
+        }, py::keep_alive<0, 1>())
+        */
         ;
-
 }
 
 #endif
